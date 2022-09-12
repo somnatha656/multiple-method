d9b9138247b06ffe7a31cc7cddaec6cd5ad2c09f
somnatha656 <somnatha656@gmail.com>
2022-09-12 22:20:39 +0100

    test

---
+++ src/Bundle/Bunk.java
@@ -0,0 +1,15 @@
+package Bundle;
+
+public class Bunk {
+    public static void main(String[] args) {
+        System.out.println("Jalpa");
+        System.out.println("My Home Address");
+        System.out.println("My Mob No");
+        System.out.println("My Work Address");
+        System.out.println("My Work Tel No");
+        
+
+        }
+    }
+
+
